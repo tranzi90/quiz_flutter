1e197d3d8fb1d1ed85a7e997d01831bf31f9757e
denis_levchenko <tranzi90@gmail.com>
2023-06-23 23:44:24 +0300

    prettify result screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/data/questions.dart';
-import 'package:quiz/questions_summary.dart';
+import 'package:quiz/questions_summary/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
